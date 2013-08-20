469dd0df2405ddd0742a73024a57b5d88b38b0b3
Paul Jan <plj126@yahoo.com>
2013-08-20 17:06:11 +0800

    fix typo

--- engine/source/platformiOS/2d/assets/FilterImageAsset.mm
+++ engine/source/platformiOS/2d/assets/FilterImageAsset.mm
@@ -183,7 +183,7 @@ void FilterImageAsset::calculateImage( void )
            if (strvalue != nullptr)
            {
               NSObject *value = [NSClassFromString([info objectForKey:kCIAttributeClass]) alloc];
-              NSString *aType = [info objectForKey:kCIAttributeType];∫
+              NSString *aType = [info objectForKey:kCIAttributeType];
               if ([value isMemberOfClass:[NSNumber class]])
               {
                  if ([aType isEqualToString:kCIAttributeTypeTime])
